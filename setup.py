d4332333ccfcc6c92f58b4a9ad9ff8e10ba9cd62
ARC4N3 <arcaneisc00l@gmail.com>
2021-04-10 22:50:50 +0530

    UPDATE setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,20 @@ def read_requirements():
         content = req.read()
         requirements = content.split('\n')
 
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md"), encoding = "utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='tweetle',
-    version='0.1',
+    version='0.1.0',
+    author = "ARC4N3",
+    author_email = "arcaneisc00l@gmail.com",
+    url = "https://github.com/4RCAN3/Tweetle",
+    description = "A CLI made to control your twitter account and get analytical data",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "MIT",
     packages=find_packages(),
     include_package_data=True,
     install_requires=read_requirements(),
@@ -17,3 +28,8 @@ setup(
     tweetle=tweetle.cli:cli
     ''',
 )
+
+'''
+Tweetle
+ARC4N3,2021
+'''
