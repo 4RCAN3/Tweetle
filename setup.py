f612cc2a0804a7b59ddefd3a2c3e1956babcdcf0
ARC4N3 <arcaneisc00l@gmail.com>
2021-04-11 02:31:07 +0530

    bug fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-
 def read_requirements():
     with open('requirements.txt') as req:
         content = req.read()
         requirements = content.split('\n')
 
-this_dir = path.abspath(path.dirname(__file__))
-with open(path.join(this_dir, "README.md"), encoding = "utf-8") as f:
-    long_description = f.read()
+def long_description():
+    with open("README.md", encoding = "utf-8") as f:
+        long_description = f.read()
+    return long_description
 
 setup(
     name='tweetle',
@@ -17,7 +17,7 @@ setup(
     author_email = "arcaneisc00l@gmail.com",
     url = "https://github.com/4RCAN3/Tweetle",
     description = "A CLI made to control your twitter account and get analytical data",
-    long_description = long_description,
+    long_description = long_description(),
     long_description_content_type = "text/markdown",
     license = "MIT",
     packages=find_packages(),
